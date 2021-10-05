aa298652d12dd54dc6920fc8762d30e5bbae2322
Ivan.pamukliyski@students.softuni.bg <Ivan.pamukliyski@students.softuni.bg>
2021-10-05 19:53:19 +0300

    I changed hello world to hello changes

--- 09. Kamino Factory/Program.cs
+++ 09. Kamino Factory/Program.cs
@@ -6,7 +6,7 @@ namespace _09._Kamino_Factory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello changes");
         }
     }
 }
